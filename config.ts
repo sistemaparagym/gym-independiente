33704f8e5864700bbbc86b9d1200eb1b2b5906b2
sistemaparagym <sistemaparagym@gmail.com>
2025-12-10 12:03:46 -0300

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,13 @@
+// Archivo: src/config.ts
+
+export const PROJECT_STATUS = {
+  // --- INTERRUPTOR PRINCIPAL (KILL SWITCH) ---
+  // true = El software funciona correctamente
+  // false = Muestra la pantalla de "Servicio Suspendido"
+  isActive: true, 
+  
+  // Datos de contacto para la pantalla de bloqueo
+  providerName: "Eduardo Ricci", 
+  providerWhatsapp: "5493825625422", // Tu número con código de país
+  providerEmail: "edur900@gmail.com"
+};
